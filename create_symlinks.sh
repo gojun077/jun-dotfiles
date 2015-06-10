76a331274ac99d88a7958f2ce6ddaa0991f571d9
Jun's D257 <gojun077@gmail.com>
2015-06-10 18:13:00 +0900

    in the middle of creating conditionals for the remaining symlinks

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -18,20 +18,20 @@ for i in $DOTFILES; do
 done
 
 
-AP_TARGETS="$HOME/.quodlibet/stations $HOME/.config/terminator/config \
-$HOME/.config/lxterminal/lxterminal.conf $HOME/.config/xfce4/terminal/terminalrc"
+QUOD="$HOME/.quodlibet"
+TMNTR="$HOME/.config/terminator/config"
+LXTM="$HOME/.config/lxterminal/lxterminal.conf"
+XFTM="$HOME/.config/xfce4/terminal/terminalrc"
 
-for i in $AP_TARGETS; do
-  if [[ -f $i && ! -L $i ]]; then
-    mv $i $i.old
-  fi
-done
 
-ln -s $HOME/dotfiles/stations $HOME/.quodlibet/stations
+if [[ -f $QUOD/stations && ! -L $QUOD/stations ]]; then
+  mv $QUOD/stations $QUOD/stations.old
+  ln -s $HOME/dotfiles/stations $QUOD/stations.old
+fi
+
 ln -s $HOME/dotfiles/terminator $HOME/.config/terminator/config
 ln -s $HOME/dotfiles/lxterminal $HOME/.config/lxterminal/lxterminal.conf
 ln -s $HOME/dotfiles/xfceTerm $HOME/.config/xfce4/terminal/terminalrc
-
 
 # Note that to create a symlink from root_bashrc to ~/root/.bashrc
 # your regular user needs to have rwx permissions on /root
@@ -43,14 +43,14 @@ ln -s $HOME/dotfiles/xfceTerm $HOME/.config/xfce4/terminal/terminalrc
 # if file exists and is not a symlink
 if [[ -f /root/.bashrc && ! -L /root/.bashrc ]]; then
   mv /root/.bashrc /root/.bashrc_old
+  ln -s $HOME/dotfiles/root_bashrc /root/.bashrc
 fi
 
-ln -s $HOME/dotfiles/root_bashrc /root/.bashrc
 
 if [[ -f /root/.vimrc && ! -L /root/.vimrc ]]; then
   mv /root/.vimrc /root/.vimrc_old
   mkdir /root/tmp #path for vim swap and temp files (root)
+  ln -s $HOME/dotfiles/vimrc /root/.vimrc
 fi
 
-ln -s $HOME/dotfiles/vimrc /root/.vimrc
 
