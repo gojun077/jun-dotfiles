37e4e54261d4e9f6b8215ce283e6fea307cac906
Jun's D257 <gojun077@gmail.com>
2015-10-12 23:45:02 +0900

    replaced symlink creation conditionals with a custom function

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -32,12 +32,6 @@ create_sym()
 }
 
 
-####################################################
-# DECLARATIONS
-####################################################
-DOTFILESP="$HOME/dotfiles"
-QUODCONF="quod_stations"
-
 
 ####################################################
 # Create Symlinks to dotfiles directly below ~/
@@ -54,89 +48,31 @@ done
 # Create Symlinks to files in subdirectories of $HOME
 ######################################################
 
-# the 4-letter variable is the original path for the conf file
-# the variable with '_TGT' appended is the original name of the conf
-# file in its original path
+#CMUS CONFIG
+#LXTERMINAL CONFIG
+create_sym "$HOME/.config/lxterminal/lxterminal.conf" "$HOME/dotfiles/lxterminal"
+#MAME CONFIG
+create_sym "$HOME/.mame/mame.ini" "$HOME/dotfiles/mame.ini"
+#OPENBOX CONFIG
+create_sym "$HOME/.config/openbox/autostart" "$HOME/dotfiles/openbox-autostart"
+#QUODLIBET CONFIG
+create_sym "$HOME/.quodlibet/stations" "$HOME/dotfiles/quod_stations"
+#TERMINATOR CONFIG
+create_sym "$HOME/.config/terminator/config" "$HOME/dotfiles/terminator"
+#XFCE CONFIG
+create_sym "$HOME/.config/xfce4/terminal/terminalrc" "$HOME/dotfiles/xfceTerm"
 
-QUOD="$HOME/.quodlibet"
-QUOD_TGT="stations"
-TMNTR="$HOME/.config/terminator"
-TMNTR_TGT="config"
-LXTM="$HOME/.config/lxterminal"
-LXTM_TGT="lxterminal.conf"
-XFTM="$HOME/.config/xfce4/terminal"
-XFTM_TGT="terminalrc"
-OPBX="$HOME/.config/openbox"
-OPBX_TGT="autostart"
-MAME="$HOME/.mame"
-MAME_TGT="mame.ini"
 
-# only rename original file and create symlink from $HOME/dotfiles
-# if the config file exists in the given path and is not a symlink
-# otw just create the symlink
+######################################################
+# Create Symlinks to files in under /root
+######################################################
 
-if [[ -f $QUOD/$QUOD_TGT && ! -L $QUOD/$QUOD_TGT ]]; then
-  mv $QUOD/$QUOD_TGT $QUOD/$QUOD_TGT.old
-  ln -s $DOTFILESP/$QUODCONF $QUOD/$QUOD_TGT
-else
-  ln -s $DOTFILESP/$QUODCONF $QUOD/$QUOD_TGT
-fi
-
-if [[ -f $TMNTR/$TMNTR_TGT && ! -L $TMNTR/$TMNTR_TGT ]]; then
-  mv $TMNTR/$TMNTR_TGT $TMNTR/$TMNTR_TGT.old
-  ln -s $HOME/dotfiles/terminator $TMNTR/$TMNTR_TGT
-else
-  ln -s $HOME/dotfiles/terminator $TMNTR/$TMNTR_TGT
-fi
-
-if [[ -f $LXTM/$LXTM_TGT.conf && ! -L $LXTM/$LXTM_TGT ]]; then
-  mv $LXTM/$LXTM_TGT $LXTM/$LXTM_TGT.old
-  ln -s $HOME/dotfiles/lxterminal $LXTM/$LXTM_TGT
-else
-  ln -s $HOME/dotfiles/lxterminal $LXTM/$LXTM_TGT
-fi
-
-if [[ -f $XFTM/$XFTM_TGT.conf && ! -L $XFTM/$XFTM_TGT ]]; then
-  mv $XFTM/$XFTM_TGT $XFTM/$XFTM_TGT.old
-  ln -s $HOME/dotfiles/xfceTerm $XFTM/$XFTM_TGT
-else
-  ln -s $HOME/dotfiles/xfceTerm $XFTM/$XFTM_TGT
-fi
-
-if [[ -f $OPBX/$OPBX_TGT.conf && ! -L $OPBX/$OPBX_TGT ]]; then
-  mv $OPBX/$OPBX_TGT $OPBX/$OPBX_TGT.old
-  ln -s $HOME/dotfiles/openbox-autostart $OPBX/$OPBX_TGT
-else
-  ln -s $HOME/dotfiles/openbox-autostart $OPBX/$OPBX_TGT
-fi
-
-if [[ -f $MAME/$MAME_TGT.ini && ! -L $MAME/$MAME_TGT ]]; then
-  mv $MAME/$MAME_TGT $MAME/$MAME_TGT.old
-  ln -s $HOME/dotfiles/mame.ini $MAME/$MAME_TGT
-else
-  ln -s $HOME/dotfiles/mame.ini $MAME/$MAME_TGT
-fi
-
-# Note that to create a symlink from root_bashrc to ~/root/.bashrc
+# NOTE: to create symlinks from root_bashrc to ~/root/.bashrc
 # your regular user needs to have rwx permissions on /root
 # you can achieve this using Access Control Lists
+#
 # setfacl -m "u:USERNAME:rwx" /root
 
-# mv original file so we can create symlink
+create_sym "/root/.bashrc" "$HOME/dotfiles/root_bashrc"
+create_sym "/root/.vimrc" "$HOME/dotfiles/vimrc"
 
-# if file exists and is not a symlink, rename it and create symlink
-# from $HOME/dotfiles
-if [[ -f /root/.bashrc && ! -L /root/.bashrc ]]; then
-  mv /root/.bashrc /root/.bashrc_old
-  ln -s $HOME/dotfiles/root_bashrc /root/.bashrc
-else
-  ln -s $HOME/dotfiles/root_bashrc /root/.bashrc
-fi
-
-if [[ -f /root/.vimrc && ! -L /root/.vimrc ]]; then
-  mv /root/.vimrc /root/.vimrc_old
-  mkdir /root/tmp #path for vim swap and temp files (root)
-  ln -s $HOME/dotfiles/vimrc /root/.vimrc
-else
-  ln -s $HOME/dotfiles/vimrc /root/.vimrc
-fi
