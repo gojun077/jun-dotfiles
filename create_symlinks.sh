ae0141deb47cb2ee24d07126d9e754db40b36d3e
fedjun <gojun077@gmail.com>
2016-03-29 11:46:07 +0900

    Added if-stmts to check if program is installed
    If a certain program is not installed, it is not necessary to install the
    associated conf files

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -51,25 +51,51 @@ done
 ######################################################
 
 #CMUS CONFIG
-create_sym "$HOME/.config/cmus/lib.pl" "$HOME/dotfiles/cmus_libpl"
+if [ -f /usr/bin/cmus ]; then
+  create_sym "$HOME/.config/cmus/lib.pl" "$HOME/dotfiles/cmus_libpl"
+fi
+
 #LXTERMINAL CONFIG
-create_sym "$HOME/.config/lxterminal/lxterminal.conf" "$HOME/dotfiles/lxterminal"
+if [ -f /usr/bin/lxterminal ]; then
+  create_sym "$HOME/.config/lxterminal/lxterminal.conf" \
+	     "$HOME/dotfiles/lxterminal"
+fi
+
 #MAME CONFIG
-mkdir -p "$HOME/.mame/ini"
-create_sym "$HOME/.mame/ini/mame.ini" "$HOME/dotfiles/mame.ini"
+if [ -f /usr/bin/sdlmame ]; then
+  mkdir -p "$HOME/.mame/ini"
+  create_sym "$HOME/.mame/ini/mame.ini" "$HOME/dotfiles/mame.ini"
+fi
+
 #OPENBOX CONFIG
 create_sym "$HOME/.config/openbox/autostart" "$HOME/dotfiles/openbox-autostart"
 #TERMINATOR CONFIG
-create_sym "$HOME/.config/terminator/config" "$HOME/dotfiles/terminator"
-#XFCE CONFIG
-create_sym "$HOME/.config/xfce4/terminal/terminalrc" "$HOME/dotfiles/xfceTerm"
-#IRSSI CONFIG
-create_sym "$HOME/.irssi/config" "$HOME/dotfiles/irssi-config"
-#QUODLIBET CONFIG
-create_sym "$HOME/.quodlibet/stations" "$HOME/dotfiles/quod_stations"
-#TODO XFCE4 PANEL XFCE TIMER CONFIG
+if [ -f /usr/bin/terminator ]; then
+  create_sym "$HOME/.config/terminator/config" "$HOME/dotfiles/terminator"
+fi
 
-#TODO XFCE4 KEYBOARD SHORTCUTS
+#XFCE CONFIG
+if [ -f /usr/bin/xfce-terminal ]; then
+  create_sym "$HOME/.config/xfce4/terminal/terminalrc" "$HOME/dotfiles/xfceTerm"
+fi
+
+#IRSSI CONFIG
+if [ -f /usr/bin/irssi ]; then
+  create_sym "$HOME/.irssi/config" "$HOME/dotfiles/irssi-config"
+fi
+
+#QUODLIBET CONFIG
+if [ -f /usr/bin/quodlibet ]; then
+  create_sym "$HOME/.quodlibet/stations" "$HOME/dotfiles/quod_stations"
+fi
+
+#TODO XFCE4 PANEL
+
+#XFCE4 KEYBOARD SHORTCUTS
+if [ -f /usr/bin/startxfce4 ]; then
+  create_sym "$HOME/.quodlibet/stations" \
+	     "$HOME/dotfiles/xfce4-keyboard-shortcuts.xml"
+fi
 
 ######################################################
 # Create Symlinks to files in under /root
