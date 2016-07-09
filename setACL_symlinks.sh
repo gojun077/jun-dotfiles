3fb525a80720227fdefcdcd8fb9290caed577df2
archjun <gojun077@gmail.com>
2016-07-09 16:25:49 +0900

    script that sets ACL/perms for use with create_symlinks.sh and other dotfiles bootstrap scripts

---
+++ setACL_symlinks.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# setACL_symlinks.sh
+# Add $USER to Access Control List (ACL) so that create_symlinks.sh
+# can create symlinks for conf files in ~/dotfiles to system
+# directories like /root, /etc, ...
+# This script should be executed as root
+
+# Last updated 2016-07-08
+# Jun Go gojun077@gmail.com
+
+DIRS=(/root
+      /etc
+      /etc/ssh
+      /etc/motion
+)
+
+DIRSR=(/etc/bitlbee
+       /etc/ansible
+)
+
+for i in ${DIRS[*]}; do
+  if ! [ -d "$i" ]; then
+    mkdir -p "$i"
+  fi
+  setfacl -m "u:USER:rwx" "$i"
+done
+
+for j in ${DIRSR[*]}; do
+  if ! [ -d "$j" ]; then
+    mkdir -p "$j"
+  fi
+  setfacl -R -m "u:USER:rwx" "$j"
+done
