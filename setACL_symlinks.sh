9d09c6029c4e054fbab3af15a83b9f49e1de087b
archjun <gojun077@gmail.com>
2016-10-06 17:33:56 +0900

    Add variables and arrays for SSH Host keys in /etc/ssh
    When adding the regular user to the ACL for system directories
    such as /etc, /etc/ssh and so on, the proper permissions for
    the SSH host keys get messed up. If the permissions on the SSH
    host keys are too lax, users trying to SSH into the host from
    remote machines will get the following error:
    
    ssh_exchange_identification: read: Connection reset by peer
    
    If you pass the -v or -vv options to ssh to debug the problem
    from the ssh client you will get the following:
    
    key_load_public: No such file or directory
    identity file /home/archjun/.ssh/id_rsa type -1
    ...
    identity file /home/archjun/.ssh/id_rsa-cert type -1
    ...
    identity file /home/archjun/.ssh/id_dsa type -1
    ...
    and so on
    
    These errors are due to incorrect file permissions on the SSH
    Host keys on the ssh host. Note that private keys must only
    be writeable by root (600, rw- --- ---) while public keys
    must have 644 perms (rw- r-- r--)

--- setACL_symlinks.sh
+++ setACL_symlinks.sh
@@ -11,6 +11,9 @@
 # USAGE ./setACL_symlinks.sh <username>
 # example: sudo ./setACL_symlinks archjun
 
+
+USER=$1
+
 DIRS=(/root
       /etc
       /etc/ansible
@@ -24,9 +27,27 @@ DIRS=(/root
 )
 
 FILES=(/var/log/motion.log
+      )
+
+DSA="/etc/ssh_host_dsa_key"
+ECDSA="/etc/ssh_host_ecdsa_key"
+ED25519="/etc/ssh_host_ed25519_key"
+RSA="/etc/ssh_host_rsa_key"
+
+HOSTPRIV=($DSA
+          $ECDSA
+          $ED25519
+          $RSA
 )
 
-if [ -z "$1" ]; then
+HOSTPUB=($DSA.pub
+         $ECDSA.pub
+         $ED25519.pub
+         $RSA
+)
+
+
+if [ -z "$USER" ]; then
   echo "Must enter name of local user for setting ACL's"
   exit 1
 fi
@@ -35,14 +56,26 @@ for i in ${DIRS[*]}; do
   if ! [ -d "$i" ]; then
     mkdir -p "$i"
   fi
-  setfacl -R -m "u:$1:rwx" "$i"
+  printf "%s\n" "### Give $USER access perm's for $i ###"
+  setfacl -R -m "u:$USER:rwx" "$i"
   getfacl "$i"
 done
 
 for j in ${FILES[*]}; do
   if [ -f "$j" ]; then
-    setfacl -m "u:$1:rwx" "$j"
+    printf "%s\n" "### Give $USER access perm's for file $j ###"
+    setfacl -m "u:$USER:rwx" "$j"
     getfacl "$j"
   fi
 done
 
+printf "%s\n" "### Set Proper Perm's on SSH Host Keys  ###"
+for k in ${HOSTPRIV[*]}; do
+  printf "%s\n" "Set $k to rw-------"
+  chmod 600 "$k"
+done
+
+for h in ${HOSTPUB[*]}; do
+  printf "%s\n" "Set $h to rw-r--r--"
+  chmod 644 "$k"
+done
