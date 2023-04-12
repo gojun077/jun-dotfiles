145cbe6f44130378ba90cac393bf025910a49ec3
Peter Koh <peter.koh@unity3d.com>
2023-04-13 00:14:25 +0900

    new macOS dotfile configs, new Internet Radio stns

--- cmus/lib.pl
+++ cmus/lib.pl
@@ -1,6 +1,9 @@
+http://79.111.119.111:8002/surfrock
 http://stream.srg-ssr.ch/m/rsj/mp3_128
 http://185.33.21.112:11038
 http://cmvs.uncool.club:2121/vinyl-stacks.m3u
+http://184.75.212.82:1025
+http://40.89.141.247:8052
 http://streams.greenhost.nl:8080/barok
 http://bpstream.hostco.de:3000
 http://strm112.1.fm/classical_mobile_aac
@@ -9,6 +12,7 @@ http://graalradio.com:8123/future
 http://ice4.somafm.com/groovesalad-128-aac
 http://jfm1.hostingradio.ru:14536/ijstream.mp3
 http://jazzradio.ice.infomaniak.ch/jazzradio-high.mp3
+http://kathy.torontocast.com:3330
 http://stream.klassikradio.de/barock/mp3-192
 http://167.114.64.181:8325
 http://121.159.140.57:8000
@@ -21,8 +25,13 @@ http://lyd.nrk.no/nrk_radio_klassisk_mp3_h
 http://procyon.shoutca.st:8098
 http://play2.organlive.com:7002/320
 http://streaming03.zfast.co.uk:8085/autodj
+http://184.95.62.170:9242
+http://192.99.4.210:3012
 http://ice.somafm.com/secretagent
 http://wshu.streamguys.org/wshu-baroque
+http://205.134.253.103:9996
+http://91.121.174.141:31802
 http://69.64.69.158:7061
 http://uk5.internet-radio.com:8011
+http://116.202.241.212:9000
 http://us5.internet-radio.com:8201
