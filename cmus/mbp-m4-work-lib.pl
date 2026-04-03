63f5e3117856f602e758609e92adb0d1fa32845a
Peter Jun Koh <peter.junkoh@cohere.com>
2026-04-03 13:32:41 +0900

    Add cmus Internet Radio station list config

---
+++ cmus/mbp-m4-work-lib.pl
@@ -0,0 +1,15 @@
+http://cmvs.uncool.club:2121/vinyl-stacks.m3u
+http://kathy.torontocast.com:2475/stream
+http://185.33.21.112:11038
+http://streams.80s80s.de/darkwave/mp3-192/
+http://streams.fluxfm.de/Chillhop/mp3-320
+http://strm112.1.fm/classical_mobile_mp3
+http://ice1.somafm.com/groovesalad-256-mp3
+http://jfm1.hostingradio.ru:14536/ijstream.mp3
+http://stream.laut.fm/lofi
+http://play.organlive.com:7000
+http://play.organlive.com:7002
+http://listen7.myradio24.com/69366
+http://ice1.somafm.com/darkzone-128-mp3
+http://play2.organlive.com:7006
+http://uk1.internet-radio.com:8355
