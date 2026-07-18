bb438907702d0735b5c640dbb08f2e78061a1193
Peter Jun Koh <gopeterjun@naver.com>
2026-07-18 22:50:59 +0900

    add Wezterm config file for Macbook Air M2 Asahilinux

---
+++ wezterm_mbair.lua
@@ -0,0 +1,22 @@
+-- original path to file: $HOME/.wezterm.lua
+-- PJ's Wezterm Config
+-- Created on: May 8 2024
+-- Created by: gopeterjun@naver.com
+-- Last Updated: Thu 16 Jul 2026
+
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.color_scheme = 'duskfox'
+-- the following font name is only valid on Linux
+--config.font = wezterm.font 'MonofurNerdFontMono'
+config.font = wezterm.font 'Monofur Nerd Font Mono'
+config.font_size = 15.0
+config.enable_tab_bar = false
+
+-- and finally, return the configuration to wezterm
+return config
